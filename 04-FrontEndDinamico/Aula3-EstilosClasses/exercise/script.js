afdf226275c2090fcc90eb04c079327685d2939d
Barbara Azevedo <barbara.azevedo@sap.com>
2026-05-17 14:44:19 -0300

    first commit

--- 04-FrontEndDinamico/Aula3-EstilosClasses/exercise/script.js
+++ 04-FrontEndDinamico/Aula3-EstilosClasses/exercise/script.js
@@ -1,10 +1,56 @@
-// Um input do tipo checkbox para que o usuário possa marcar (ou desmarcar) um produto da lista como comprado.
-// Uma span para incluir o nome do produto, ou seja, de fato o texto que o usuário digitar.
-// E, por fim, um button que vai ter a função de remover o produto da lista. Ao invés de clicar sobre o produto para remover ele da lista, iremos remover apenas se clicarmos no botão.
-
+//REFATORANDO
 const inputLista = document.querySelector("input#input");
 const button = document.querySelector("button");
 const lista = document.querySelector("#lista");
+
+function toggleConluido(event) {
+  const checkboxItem = event.target;
+  const spanItem = document.createElement("span");
+  const concluido = checkbox.checked;
+  spanItem.style.textDecoration = concluido ? "line-through" : "none";
+  spanItem.style.opacity = concluido ? "0.5" : "1";
+  checkboxItem.style.opacity = concluido ? "0.5" : "1";
+}
+
+function criarItem(texto) {
+  
+}
+
+function adicionarItem() {
+  if (inputLista.value.length <= 2 ){
+    window.alert("O item deve ter mais que 2 letras")
+  } else {
+    lista.appendChild(criarItem(inputLista.value));
+    inputLista.value = ""; //serve para limpar o campo de texto
+  }
+}
+
+function removerItem(event) {
+  lista.removeChild(event.target.parentElement);
+}
+
+document.querySelector("#btnRemove").addEventListener("click", removerItem)
+
+// ---------------------- ADICIONANDO ESTILO ---------------------
+
+const styleTag = document.createElement("style");
+
+styleTag.innerHTML = `
+  body {
+    background-color: #ece0e0;
+    color: #000000;
+    margin: 2rem;
+    width: 50%;
+  }
+`;
+
+document.querySelector("head").appendChild(styleTag)
+
+//Estilo checked(linha por cima) para itens concluídos
+document.getElementById("checkboxFixed").addEventListener("click", toggleConluido);
+
+
+
 
 button.addEventListener("click", (event) => {
 
@@ -18,19 +64,6 @@ button.addEventListener("click", (event) => {
 
     textItem.textContent = inputLista.value;    
     checkboxItem.setAttribute("type", "checkbox");
-
-    //Inserir estulo ao checkbox
-    checkboxItem.addEventListener("click", (event) => {
-      if (event.target.checked) {
-        textItem.style.textDecoration = "line-through";
-        textItem.style.opacity = 0.6;
-        checkboxItem.style.opacity = 0.6;
-      } else {
-        textItem.style.textDecoration = "none";
-        textItem.style.opacity = 1;
-        checkboxItem.style.opacity = 1;
-      }
-    })
 
     removeButtonItem.textContent = "x";
 
@@ -54,30 +87,4 @@ function removerItem(event) {
 
 document.querySelector("#btnFixedRemove").addEventListener("click", removerItem)
 
-// ---------------------- ADICIONANDO ESTILO ---------------------
-const styleTag = document.createElement("style");
 
-styleTag.innerHTML = `
-  body {
-    background-color: #ece0e0;
-    color: #000000;
-    margin: 2rem;
-    width: 50%;
-  }
-`;
-
-const headTag = document.querySelector("head");
-
-headTag.appendChild(styleTag)
-
-//Estilo checked(linha por cima) para itens concluídos
-const checkboxFixed = document.getElementById("checkboxFixed");
-const spanFixed = document.getElementById("spanFixed");
-
-checkboxFixed.addEventListener("click", (event) => {
-    if (event.target.checked) {
-      spanFixed.style.textDecoration = "line-through";
-    } else {
-      spanFixed.style.textDecoration = "none"
-    }
-})
